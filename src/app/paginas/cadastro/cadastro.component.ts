923182e9c37679eef176426fb66d7a70e73f5b29
AcauRangel <acauarangel17@gmail.com>
2025-07-06 23:34:42 -0300

    exemplo http request

--- src/app/paginas/cadastro/cadastro.component.ts
+++ src/app/paginas/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../componentes/header/header.component';
 import { FooterComponent } from '../../componentes/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,8 +12,41 @@ import { RouterLink } from '@angular/router';
 })
 export class CadastroComponent {
 
+  constructor(private readonly http: HttpClient) {}
+
   onLogin(): void {
     console.log('Login clicado');
+  }
+
+  submit(): void {
+    //rota post
+    const body = {"login": "oiwufgyhrbe", "password": "ruwkgyb"}
+
+    this.http.post('http://localhost:3001/login', body, {
+      headers: new HttpHeaders({ 'Accept': 'application/json' }),
+      withCredentials: false
+    }).subscribe({
+      next: (res) => {
+        // sua lógica
+      },
+      error: (err) => {
+        //sua lógica
+      }
+    });
+
+
+    //rota get
+    this.http.get('http://localhost:3001/login', {
+      headers: new HttpHeaders({ 'Accept': 'application/json' }),
+      withCredentials: false
+    }).subscribe({
+      next: (res) => {
+        // sua lógica
+      },
+      error: (err) => {
+        //sua lógica
+      }
+    });
   }
 
 }
